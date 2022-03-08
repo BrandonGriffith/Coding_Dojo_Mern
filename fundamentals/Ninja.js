e0718e61aac961ac33200457a2572d45caad0589
BrandonGriffith <BrandonGriffith92@gmail.com>
2022-03-08 14:06:10 -0500

    updated

--- fundamentals/Ninja.js
+++ fundamentals/Ninja.js
@@ -22,3 +22,23 @@ const ninja1 = new Ninja("Hyabusa");
 ninja1.sayName();
 ninja1.drinkSake();
 ninja1.showStats();
+
+
+
+class Sensei extends Ninja{
+    constructor(name){
+        super();
+        this.name = name;
+        this.wisdom = 10;
+    }
+    speakWisdom(){
+        console.log("What one programmer can do in one month, two programmers can do in two months.");
+    }
+}
+
+
+
+
+const superSensei = new Sensei("Master Splinter");
+superSensei.speakWisdom();
+superSensei.showStats();
