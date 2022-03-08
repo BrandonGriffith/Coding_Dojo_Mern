fc2d1a90f1993303849d4b136134fbd36548136a
BrandonGriffith <BrandonGriffith92@gmail.com>
2022-03-07 22:59:01 -0500

    updated

---
+++ fundamentals/Destructuring.js
@@ -0,0 +1,63 @@
+// const cars = ['Tesla', 'Mercedes', 'Honda']
+// const [ randomCar ] = cars
+// const [ ,otherRandomCar ] = cars
+// //Predict the output
+// console.log(randomCar)
+// console.log(otherRandomCar)
+// //randomCar = Tesla
+// //otherRandomCar = Marcedes
+
+
+// const employee = {
+//     name: 'Elon',
+//     age: 47,
+//     company: 'Tesla'
+// }
+// const { name: otherName } = employee;
+// //Predict the output
+// console.log(name);
+// console.log(otherName);
+// //error the name of the const is otherName
+
+
+// const person = {
+//     name: 'Phil Smith',
+//     age: 47,
+//     height: '6 feet'
+// }
+// const password = '12345';
+// const { password: hashedPassword } = person;  
+// //Predict the output
+// console.log(password);
+// console.log(hashedPassword);
+// //password = 12345
+// //hashedPassword is undefined because person has no password
+
+
+// const numbers = [8, 2, 3, 5, 6, 1, 67, 12, 2];
+// const [,first] = numbers;
+// const [,,,second] = numbers;
+// const [,,,,,,,,third] = numbers;
+// //Predict the output
+// console.log(first == second);
+// console.log(first == third);
+// //false 2 does not equal 5
+// //true 2 does equal 2
+
+
+const lastTest = {
+    key: 'value',
+    secondKey: [1, 5, 1, 8, 3, 3]
+}
+const { key } = lastTest;
+const { secondKey } = lastTest;
+const [ ,willThisWork] = secondKey;
+//Predict the output
+console.log(key);
+console.log(secondKey);
+console.log(secondKey[0]);
+console.log(willThisWork);
+//key = value because it is lastTest.key
+//secondKey = [1,5,1,8,3,3] because it is lastTest.secondKey
+//1 because it is index 0 of secondKey arr
+//5 because it is the index 1 of secondKey
