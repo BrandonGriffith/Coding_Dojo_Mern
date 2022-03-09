df69e65fe4ce4e477e45a0ce9b232ca39452ab21
BrandonGriffith <BrandonGriffith92@gmail.com>
2022-03-09 15:46:51 -0500

    updated

---
+++ react/prop-it-up/src/components/PersonCard.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class PersonCard extends Component {
+    render(){
+        return(
+            <div>
+                <h1>{this.props.lastName}, {this.props.firstName}</h1>
+                <p>Age: ${this.props.age}</p>
+                <p>Hair Color: {this.props.HairColor}</p>
+            </div>
+        )
+    }
+}
+export default PersonCard;
