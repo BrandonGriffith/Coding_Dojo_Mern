538871e29d42f2578b9e03ffc9ede85a422a3b75
BrandonGriffith <BrandonGriffith92@gmail.com>
2022-03-18 00:55:55 -0400

    updated

--- react/poke-api-axios/src/components/PokeApi.jsx
+++ react/poke-api-axios/src/components/PokeApi.jsx
@@ -1,25 +1,32 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import axios from 'axios';
 
 const PokeApi = () => {
     let [pokeList, setPokeList] = useState([]);
+    let [pokeSearch, setPokeSearch] = useState("");
 
-    const getPoke = () => {
+    useEffect(() => {
+        console.log("running");
         axios.get("https://pokeapi.co/api/v2/pokemon?limit=1000")
         .then((response) => {console.log(response.data.results); setPokeList(response.data.results)})
         .catch((err)=>{
             console.log("error when fetching. something went wrong. ", err )
-        })
-    }
+        }
+        )},[])
+
     return(
         <>
-        <button onClick={getPoke} className="btn btn-success m-3">Click me to get the Pokemon!!!</button>
+        <h1>Search for a pokemon!!!: <input onChange={(e)=>setPokeSearch(e.target.value.toLowerCase()) } type="text" placeholder='Search...'/></h1>
         {
-            pokeList.map((pokeObj, index) => {
+            pokeList.filter((pokeObj)=>{
+                return pokeObj.name
+                .toLowerCase()
+                .includes(pokeSearch);
+            }).map((pokeObj, index) => {
                 return <div key={index}>
                     <h1>{pokeObj.name}</h1>
-                </div>
-            })
+                    </div>
+                })
         }
         </>
     )
